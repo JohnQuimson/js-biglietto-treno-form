daa1140ed115992ac967550ad33596f57fe2e7c6
JohnQuimson <john.quimson777@gmail.com>
2023-11-07 17:13:06 +0100

    feat: section top

--- js/main.js
+++ js/main.js
@@ -5,28 +5,33 @@ const prezzoKilometro = 0.21;
 const scontoMinorenni = 0.8;
 const scontoOver65 = 0.6;
 
-//INPUT
-/*const kilometri = Number(prompt('Distanza in km: '));
-console.log(kilometri + ' km');
-const eta = Number(prompt('Età del passeggero: '));
-console.log(eta + ' anni');*/
+const btnGenera = document.getElementById('genera');
+btnGenera.addEventListener('click', function () {
+  //INPUT
+  const nome = document.getElementById('nome').value;
+  const kilometri = Number(document.getElementById('kilometri').value);
+  const eta = document.getElementById('eta').value;
 
-//VERIFICA NUMERI
-if (!isNaN(kilometri) && !isNaN(eta)) {
-  //Calcolo prezzo
-  let prezzo = prezzoKilometro * kilometri;
-  console.log('Prezzo biglietto: ' + prezzo.toFixed(2) + '€');
+  console.log(nome);
+  console.log(kilometri);
+  console.log(eta);
 
-  //CONDIZIONI
-  if (eta < 18) {
-    prezzo = prezzo * scontoMinorenni;
-    console.log('Prezzo con sconto minorenni: ' + prezzo.toFixed(2) + '€');
-  } else if (eta >= 65) {
-    prezzo = prezzo * scontoOver65;
-    console.log('Prezzo con sconto Over 65: ' + prezzo.toFixed(2) + '€');
+  //VERIFICA NUMERI
+  if (!isNaN(kilometri)) {
+    //Calcolo prezzo
+    let prezzo = prezzoKilometro * kilometri;
+    console.log('Prezzo biglietto: ' + prezzo.toFixed(2) + '€');
+
+    //CONDIZIONI
+    if (eta == 'minorenne') {
+      prezzo = prezzo * scontoMinorenni;
+    } else if (eta == 'over65') {
+      prezzo = prezzo * scontoOver65;
+    }
   } else {
-    console.log('Non vengono applicati sconti.');
+    console.log('Errore');
   }
-} else {
-  console.log('Errore');
-}
+
+  //INNER HTML
+  document.getElementById;
+});
